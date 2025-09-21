baf952c6f891555037a6972a9ba854c5a774349d
Waqar Arshad <waqararshadwma15@gmail.com>
2025-09-21 23:56:16 +0500

    Signup

--- src/app.js
+++ src/app.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const app = express();
 const { connectDB } = require('./config/database');
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 app.use("/signup", (req, res) => {
     try {
-        const { firstName, lastName, email, password } = req.body
-        if(!firstName || lastName){
-            throw new error('Name is Invalid')
-        }else{
-            res.send(firstName+lastName)
-        }
-    } catch (err) {
-        console.log("Error in Signup Api", err)
+        // signup logic here
+        res.send("Signup route");
+    } catch (error) {
+        console.log("Error", error);
+        res.status(500).send("Internal Server Error");
     }
-
-})
+});
 
 connectDB().then(() => {
     console.log('Database connected successfully');
