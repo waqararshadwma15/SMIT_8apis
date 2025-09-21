4f0a511d2e7dfaf641ae7e87defa6ba611e0c9c9
Waqar Arshad <waqararshadwma15@gmail.com>
2025-09-21 20:16:44 +0500

    Second Commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
-const {connectDB} = require('./config/database');
+const { connectDB } = require('./config/database');
 
 app.use(express.json());
-app.use("/",(req,res)=>{
-    res.send("Hello World!")
+app.use("/signup", (req, res) => {
+    try {
+        const { firstName, lastName, email, password } = req.body
+        if(!firstName || lastName){
+            throw new error('Name is Invalid')
+        }else{
+            res.send(firstName+lastName)
+        }
+    } catch (err) {
+        console.log("Error in Signup Api", err)
+    }
 
 })
 
